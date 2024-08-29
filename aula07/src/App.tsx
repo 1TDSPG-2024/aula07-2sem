5f50c897ed326e539fb065238ac8de566c3bead7
britojulia <juliabri1234@gmail.com>
2024-08-29 12:21:06 -0300

    Initial commit

--- aula07/src/App.tsx
+++ aula07/src/App.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Rodape from "./components/Rodape/Rodape";
 import { MainContent } from "./style/styled";
+import "./App.css"
 
 export default function App(){
   return(
